eb33c2adc29516d0acea92bcbdd60fb48c4da9af
Hazunye <160357612+Hazunye@users.noreply.github.com>
2024-02-17 17:25:52 -0300

    Add files via upload

--- main2.js
+++ main2.js
@@ -3,7 +3,6 @@
 var UI_Favicon = 1;				// [&] channel favicon
 var UI_MiniLogo = 0;			// [&] small channel logo/avatar in the top navbar
 var UI_ChannelName = 1;			// [&] channel custom brand name
-							// [&] additional header dropdown menu
 var UI_RemoveHomeMenu = 1;		// removing 'Home' menu from the header
 var UI_TitleBarDescription = 1;	// [&] custom title bar description
 var UI_UserCommands = 1;		// [&] additional commands in the chat window
