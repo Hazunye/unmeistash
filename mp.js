953d0e11f345f006c016a9680c951887f8f4195c
Hazunye <160357612+Hazunye@users.noreply.github.com>
2024-07-08 01:43:27 -0300

    Update mp.js

--- mp.js
+++ mp.js
@@ -442,6 +442,23 @@ fixUserlistHover();
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+function prepareFilters() {
+	str = '{"name":"Display webm","source":"http(.+?):webm","flags":"gi",'
+		+ '"replace":"<a class=\\"webm\\" href=\\"http$1\\" target=\\"_blank\\">http$1</a>","active":true,"filterlinks":true},'
+		+ '{"name":"Image","source":"http(.+?):pic","flags":"i",'
+	callback = function(data) {
+		socket.listeners("chatFilters").splice(
+			socket.listeners("chatFilters").indexOf(callback)
+		);
+		temp = JSON.stringify(data);
+		comma = (temp.length!="2") ? ',' : '';
+		$("#cs-chatfilters-exporttext").val(temp.substring(0, temp.length-1) + comma + str);
+	};
+
+	socket.once("chatFilters", callback);
+	socket.emit("requestChatFilters");
+}
+
 // change title bar description
 function changeTitle() {
 	title = $("#currenttitle").text();
